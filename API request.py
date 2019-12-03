2241bf405da2e070d243953503fc16ba47d8952a
mated <elmatte@gmail.com>
2019-12-03 19:28:39 +0100

    zapisywanie api do pliku

--- API request.py
+++ API request.py
@@ -1,12 +1,19 @@
-import requests, json
+import requests, json                                       #zaimportowane pakiety
 response = requests.get("https://reqres.in/api/users")      #zdefiniowana zmienna jako funkcja
-print(response.status_code)
+print(response.status_code)                                 #metoda zwraca status code zmiennej (response)
 #print(response.json())
 
 def json_print(x):                                              #funkcja do formatowania jsona
-    text = json.dumps(x, sort_keys = True, indent = 3)
-    print(text)
+    txt_response = json.dumps(x, sort_keys = True, indent = 3)          #converts python object (x) to json
+    print(txt_response)
 
-json_print(response.json())                             #wywolana funkcja stworzona powyzej
+def api_file_save (x):                                          #funkcja do zapisania api do pliku
+    api_file = open("api_file.txt", "w")                        #stworzenie pliku
+    txt_response = json.dumps(x, sort_keys = True, indent = 3)
+    #api_file.write(x.text)                                     #parametr 'text' funkcji requests zeby zmienic do str
+    api_file.write(txt_response)
+    api_file.close()
 
-print ('test gita')
+#json_print(response.json())                             #wywolana funkcja stworzona powyzej
+
+api_file_save(response.json())                                  #metoda .json() parsuje zmienna do formatu json
